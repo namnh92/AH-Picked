a4d4427e969c54fdd4d92984c888012274468bbb
Nguyễn Nam <namnguyen.hypertech@gmail.com>
2019-06-15 13:26:25 +0700

    init project

---
+++ vn-pickup/vn-pickup/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  vn-pickup
+//
+//  Created by Nguyễn Nam on 6/15/19.
+//  Copyright © 2019 Nguyễn Nam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
